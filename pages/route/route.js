c9119f4a28fac971948e53b48999264ce9e2c0c1
suveng <suveng@163.com>
2018-10-18 21:03:29 +0800

    手写路径

--- pages/route/route.js
+++ pages/route/route.js
@@ -38,13 +38,123 @@ Page({
     that2 = this;
 
     point.push({
-      latitude: 23.176167,
-      longitude: 113.480819
+      latitude: 23.147187,  
+      longitude: 113.494778
     });
     point.push({
-      latitude: 23.175753,
-      longitude: 113.481688
+      latitude: 23.149436, 
+      longitude: 113.496065
     });
+    point.push({
+      latitude: 23.152672, 
+      longitude: 113.498940
+    });
+    point.push({
+      latitude: 23.153461, 
+      longitude: 113.499799
+    });
+    point.push({
+      latitude: 23.154763, 
+      longitude: 113.501859
+    });
+    point.push({
+      latitude: 23.154763, 
+      longitude: 113.504348
+    });
+    point.push({
+      latitude: 23.154724, 
+      longitude: 113.504477
+    });
+        point.push({
+          latitude: 23.153974, 
+          longitude: 113.505120
+    });
+    point.push({
+      latitude: 23.153974, 
+      longitude: 113.505120
+    });
+    point.push({
+      latitude: 23.153974, 
+      longitude: 113.505120
+    });
+    point.push({
+      latitude: 23.150699, 
+      longitude: 113.507609
+    });
+    point.push({
+      latitude: 23.149910, 
+      longitude: 113.508081
+    });
+    point.push({
+      latitude: 23.149041, 
+      longitude: 113.508875
+    });
+    point.push({
+      latitude: 23.148390, 
+      longitude: 113.509326
+    });
+    point.push({
+      latitude: 23.148311, 
+      longitude: 113.509219
+    });
+    point.push({
+      latitude: 23.148252, 
+      longitude: 113.509219
+    });
+    point.push({
+      latitude: 23.148371, 
+      longitude: 113.507674
+    });
+    point.push({
+      latitude: 23.148430, 
+      longitude: 113.507566
+    });
+    point.push({
+      latitude: 23.148430, 
+      longitude: 113.507116
+    });
+    point.push({
+      latitude: 23.148351, 
+      longitude: 113.506837
+    });
+    point.push({
+      latitude: 23.148410,
+      longitude: 113.506429
+    });
+    point.push({
+      latitude: 23.147384, 
+      longitude: 113.506365
+    });
+    point.push({
+      latitude: 23.147305, 
+      longitude: 113.506343
+    });
+    point.push({
+      latitude: 23.147364, 
+      longitude: 113.505871
+    });
+    point.push({
+      latitude: 23.147404, 
+      longitude: 113.505657
+    });
+    point.push({
+      latitude: 23.147700,
+      longitude: 113.505528
+    });
+    point.push({
+      latitude: 23.147798, 
+      longitude: 113.505528
+    });
+    point.push({
+      latitude: 23.147562,
+      longitude: 113.504906
+    });
+    point.push({
+      latitude: 23.147345,
+      longitude: 113.504283
+    });
+    
+    
 
 
     wx.getLocation({
