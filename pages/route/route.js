b7147f63d9668aa7cdfdfb84d6b6a23f11467060
suveng <suveng@163.com>
2019-02-18 18:20:52 +0800

    轨迹模块-进入跳转到坐标起点

--- pages/route/route.js
+++ pages/route/route.js
@@ -1,120 +1,145 @@
 var point = [];
 var that2;
 
-function drawline() {
-  that2.setData({
-    polyline: [{
-      points: point,
-      color: '#000000',
-      width: 4,
-      dottedLine: false
-    }]
-  });
-}
 
-//获取经纬度
-function getlocation() {
-  var lat, lng;
-  wx.getLocation({
-    type: 'gcj02',
-    success: function(res) {
-      lat = res.latitude;
-      lng = res.longitude;
-      point.push({
-        latitude: lat,
-        longitude: lng
-      });
-      console.log(point);
-    }
-  });
-}
+
 
 Page({
+  // 页面数据
   data: {
     polyline: [],
+    lo: 114.494778,
+    la: 23.147187
   },
-
+  // 生命周期函数 加载完毕后执行
   onLoad: function() {
+    // 1. 设置 地理数据
+    this.pointSetData(point);
+    // 2. 赋值 this page 变量给that
     that2 = this;
+    // 3.创建mapctx
+    this.mapCtx = wx.createMapContext('map');
+    // 4. 画轨迹
+    this.drawline(that2);
 
+  },
+  drawline:function (that) {
+    // 1. 设置地图 wxml 的lo la
+    that2.setData({
+      lo: point[0].longitude,
+      la: point[0].latitude
+    });
+    // that2.mapCtx.longitude = point[0].longitude;
+    // that2.mapCtx.latitude = point[0].latitude;
+    // console.log(that.mapCtx);
+    // that.mapCtx.moveToLocation();
+
+
+    that2.setData({
+      polyline: [{
+        points: point,
+        color: '#000000',
+        width: 4,
+        dottedLine: false
+      }]
+    });
+  },
+  // 自定义函数
+  start: function() {
+    this.timer = setInterval(repeat, 1000);
+
+    function repeat() {
+      console.log('re');
+      getlocation();
+      var that = this;
+      this.drawline(that);
+    }
+  },
+  // 自定义函数
+  end: function() {
+    console.log('end');
+    clearInterval(this.timer);
+  },
+  // 设置数据-自定义函数
+  pointSetData(point) {
     point.push({
-      latitude: 23.147187,  
+      latitude: 23.147187,
       longitude: 113.494778
     });
     point.push({
-      latitude: 23.149436, 
+      latitude: 23.149436,
       longitude: 113.496065
     });
     point.push({
-      latitude: 23.152672, 
+      latitude: 23.152672,
       longitude: 113.498940
     });
     point.push({
-      latitude: 23.153461, 
+      latitude: 23.153461,
       longitude: 113.499799
     });
     point.push({
-      latitude: 23.154763, 
+      latitude: 23.154763,
       longitude: 113.501859
     });
     point.push({
-      latitude: 23.154763, 
+      latitude: 23.154763,
       longitude: 113.504348
     });
     point.push({
-      latitude: 23.154724, 
+      latitude: 23.154724,
       longitude: 113.504477
     });
-        point.push({
-          latitude: 23.153974, 
-          longitude: 113.505120
-    });
     point.push({
-      latitude: 23.153974, 
+      latitude: 23.153974,
       longitude: 113.505120
     });
     point.push({
-      latitude: 23.153974, 
+      latitude: 23.153974,
       longitude: 113.505120
     });
     point.push({
-      latitude: 23.150699, 
+      latitude: 23.153974,
+      longitude: 113.505120
+    });
+    point.push({
+      latitude: 23.150699,
       longitude: 113.507609
     });
     point.push({
-      latitude: 23.149910, 
+      latitude: 23.149910,
       longitude: 113.508081
     });
     point.push({
-      latitude: 23.149041, 
+      latitude: 23.149041,
       longitude: 113.508875
     });
     point.push({
-      latitude: 23.148390, 
+      latitude: 23.148390,
       longitude: 113.509326
     });
     point.push({
-      latitude: 23.148311, 
+      latitude: 23.148311,
       longitude: 113.509219
     });
     point.push({
-      latitude: 23.148252, 
+      latitude: 23.148252,
       longitude: 113.509219
     });
     point.push({
-      latitude: 23.148371, 
+      latitude: 23.148371,
       longitude: 113.507674
     });
     point.push({
-      latitude: 23.148430, 
+      latitude: 23.148430,
       longitude: 113.507566
     });
     point.push({
-      latitude: 23.148430, 
+      latitude: 23.148430,
       longitude: 113.507116
     });
     point.push({
-      latitude: 23.148351, 
+      latitude: 23.148351,
       longitude: 113.506837
     });
     point.push({
@@ -122,19 +147,19 @@ Page({
       longitude: 113.506429
     });
     point.push({
-      latitude: 23.147384, 
+      latitude: 23.147384,
       longitude: 113.506365
     });
     point.push({
-      latitude: 23.147305, 
+      latitude: 23.147305,
       longitude: 113.506343
     });
     point.push({
-      latitude: 23.147364, 
+      latitude: 23.147364,
       longitude: 113.505871
     });
     point.push({
-      latitude: 23.147404, 
+      latitude: 23.147404,
       longitude: 113.505657
     });
     point.push({
@@ -142,7 +167,7 @@ Page({
       longitude: 113.505528
     });
     point.push({
-      latitude: 23.147798, 
+      latitude: 23.147798,
       longitude: 113.505528
     });
     point.push({
@@ -153,33 +178,5 @@ Page({
       latitude: 23.147345,
       longitude: 113.504283
     });
-    
-    
-
-
-    wx.getLocation({
-      type: 'gcj02',
-      success: function(res) {
-        that2.setData({
-          longitude: res.longitude,
-          latitude: res.latitude,
-        });
-      }
-    });
-    drawline();
-  },
-
-  start: function() {
-    this.timer = setInterval(repeat, 1000);
-
-    function repeat() {
-      console.log('re');
-      getlocation();
-      drawline();
-    }
-  },
-  end: function() {
-    console.log('end');
-    clearInterval(this.timer);
   }
 });
